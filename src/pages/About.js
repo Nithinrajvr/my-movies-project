993183154fad6eb8db6c4541129195cd98f78469
Nithinraj <nithinrajvr7@gmail.com>
2022-03-29 19:20:14 +0200

    adding in sanity studio

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title"> about us</h1>
+      <p>
+        Tempor esse id amet qui elit ut proident quis exercitation veniam
+        deserunt irure enim. Enim officia ipsum ipsum ad deserunt nostrud
+        eiusmod deserunt esse. Amet incididunt eiusmod id reprehenderit. Ex duis
+        adipisicing veniam sunt minim incididunt exercitation. Enim eiusmod
+        labore sint minim ea do consequat reprehenderit deserunt nulla ad
+        nostrud. Minim esse amet consequat consectetur adipisicing eiusmod
+        labore proident. Cupidatat pariatur nostrud aliquip ad officia ipsum
+        aliqua eu.
+      </p>
+    </section>
+  );
+};
+
+export default About;
